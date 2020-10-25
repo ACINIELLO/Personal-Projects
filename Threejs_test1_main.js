b0e34e086de4a273705360b1ba29d33ee9849c7b
Alessandro <33810424+ACINIELLO@users.noreply.github.com>
2020-10-25 17:56:58 -0400

    Add files via upload

---
+++ Threejs_test1_main.js
@@ -0,0 +1,22 @@
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+
+const scene  = new THREE.Scene(); // what and where is being rendered
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1,1000); //(camera deg, AR, near plane(too close wont be displayed), far plane(too far wont be displayed))  
+
+var renderer = new THREE.WebGLRenderer({antialias: true});// antialias to make result not as jagged
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setClearColor("#e5e5e5"); // set background colour(via HEX)
+
+//render into html doc: 
+document.body.appendChild(renderer.domElement);
+
+// create VR access: 
+document.body.appendChild(VRButton.createButton(renderer));
+renderer.xr.enabled = true;
+
+renderer.setAnimationLoop(function () {
+
+    renderer.render(scene, camera);
+
+});
